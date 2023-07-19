0862e367fc61e644861c482a1806c9de887d00bf
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-07-19 17:35:38 +0900

    Initial Commit

---
+++ PrecticeRestAPI/PrecticeRestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrecticeRestAPIApp.swift
+//  PrecticeRestAPI
+//
+//  Created by 渡邊魁優 on 2023/07/19.
+//
+
+import SwiftUI
+
+@main
+struct PrecticeRestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
